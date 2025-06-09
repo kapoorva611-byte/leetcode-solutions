618a6376973ae8c8826bb2aa7506a492034cbb45
Apoorva Kumari <kapoorva611@gmail.com>
2025-06-09 07:36:09 +0000

    sync: leetcode solution Runtime - 0 ms (100.00%), Memory - 10.1 MB (11.59%)

---
+++ 0242-valid-anagram/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+        unordered_map<char,int>m;
+        for(int i=0;i<s.size();i++){
+            if(m.count(s[i])){
+                m[s[i]]++;
+            }else{
+                m[s[i]]=1;
+            }
+        }
+        for(int i=0;i<t.size();i++){
+            if(m.count(t[i])){
+                m[t[i]]--;
+                if(m[t[i]]==0){
+                    m.erase(t[i]);
+                }
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
