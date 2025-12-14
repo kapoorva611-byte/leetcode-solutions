180561129cac421d7251855edb0933c7db95ba8b
Apoorva Kumari <kapoorva611@gmail.com>
2025-12-14 06:45:26 +0000

    sync: leetcode solution Runtime - 0 ms (100.00%), Memory - 17.7 MB (10.43%)

---
+++ 2137-final-value-of-variable-after-performing-operations/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int res=0;
+        for(int i=0;i<operations.size();i++){
+            if(operations[i]=="++X"||operations[i]=="X++"){
+                res=res+1;
+            }
+            if(operations[i]=="--X"||operations[i]=="X--"){
+                res=res-1;
+            }
+        }
+        return res;
+    }
+};
