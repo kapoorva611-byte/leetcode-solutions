99324e0d16c76d7d7ff38a340c5daeed14796c0b
Apoorva Kumari <kapoorva611@gmail.com>
2026-03-13 19:07:32 +0000

    sync: leetcode solution Runtime - 0 ms (100.00%), Memory - 9.7 MB (49.96%)

--- 0242-valid-anagram/solution.cpp
+++ 0242-valid-anagram/solution.cpp
@@ -4,21 +4,14 @@ public:
         if(s.size()!=t.size()){
             return false;
         }
-        unordered_map<char,int>m;
+        int freq[26]={0};
         for(int i=0;i<s.size();i++){
-            if(m.count(s[i])){
-                m[s[i]]++;
-            }else{
-                m[s[i]]=1;
-            }
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+
         }
-        for(int i=0;i<t.size();i++){
-            if(m.count(t[i])){
-                m[t[i]]--;
-                if(m[t[i]]==0){
-                    m.erase(t[i]);
-                }
-            }else{
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0){
                 return false;
             }
         }
