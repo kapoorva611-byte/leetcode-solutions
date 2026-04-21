0b5cf0fa168a5d828191cc3f48e09770874a97e5
Apoorva Kumari <kapoorva611@gmail.com>
2026-04-21 17:17:50 +0000

    sync: leetcode solution Runtime - 2 ms (71.70%), Memory - 8.3 MB (17.72%)

---
+++ 0767-prime-number-of-set-bits-in-binary-representation/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+        unordered_set<int> primes = {2,3,5,7,11,13,17,19,23,29,31};
+        
+        int count = 0;
+        for (int i = left; i <= right; i++) {
+            int bits = __builtin_popcount(i);
+            if (primes.count(bits)) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
