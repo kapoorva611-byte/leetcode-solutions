35ac27be9e26165d778c649e5aef50966d078f2b
Apoorva Kumari <kapoorva611@gmail.com>
2026-07-12 03:05:32 +0000

    sync: leetcode solution Runtime - 0 ms (100.00%), Memory - 10.3 MB (12.04%)

---
+++ 4357-number-of-elapsed-seconds-between-two-times/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int secondsBetweenTimes(string startTime, string endTime) {
+        auto toseconds=[](string t){
+            int h=stoi(t.substr(0,2));
+            int m=stoi(t.substr(3,2));
+            int s=stoi(t.substr(6,2));
+            return h*3600+m*60+s;
+            
+        };
+        return toseconds(endTime)-toseconds(startTime);
+    }
+};
