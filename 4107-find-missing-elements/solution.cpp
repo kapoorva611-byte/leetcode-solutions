19d5898fc0c0633c77cedb93041763187a0fe57f
Apoorva Kumari <kapoorva611@gmail.com>
2026-08-04 17:38:34 +0000

    sync: leetcode solution Runtime - 9 ms (9.61%), Memory - 33.9 MB (7.65%)

---
+++ 4107-find-missing-elements/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<int>ans;
+        int start=nums[0];
+        int end=nums[n-1];
+        unordered_map<int,int>mpp;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]];
+        }
+        for(int i=start;i<=end; i++){
+            if(mpp.find(i)==mpp.end()){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
