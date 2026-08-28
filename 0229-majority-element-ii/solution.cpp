4b979e35d8f4807cd60f93b6c320f179174a9b63
Apoorva Kumari <kapoorva611@gmail.com>
2026-08-28 18:56:59 +0000

    sync: leetcode solution Runtime - 8 ms (29.12%), Memory - 26.5 MB (17.05%)

--- 0229-majority-element-ii/solution.cpp
+++ 0229-majority-element-ii/solution.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int>m;
-        for(int i=0;i<nums.size();i++){
-            if(m.count(nums[i])){
-                m[nums[i]]++;
-            }else{
-                m[nums[i]]=1;
-            }
+        unordered_map<int,int> freq;
+        
+        for(int x : nums) {
+            freq[x]++;
         }
-        vector<int>res;
-        for(auto p:m){
-            if(p.second>(nums.size()/3)){
+        
+        vector<int> res;
+        int n = nums.size();
+        
+        for(auto &p : freq) {
+            if(p.second > n / 3) {
                 res.push_back(p.first);
             }
         }
+        
         return res;
     }
 };
