edaccd8fdda38ac298d134eb2e418a306aa6bca8
Apoorva Kumari <kapoorva611@gmail.com>
2026-09-03 19:50:08 +0000

    sync: leetcode solution Runtime - 0 ms (100.00%), Memory - 9.5 MB (99.12%)

--- 0118-pascals-triangle/solution.cpp
+++ 0118-pascals-triangle/solution.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> res(numRows);
-
-        for (int i = 0; i < numRows; i++) {
-            res[i].resize(i + 1);
-            res[i][0] = res[i][i] = 1;  // first and last elements are always 1
-
-            // compute middle elements using previous row
-            for (int j = 1; j < i; j++) {
-                res[i][j] = res[i - 1][j - 1] + res[i - 1][j];
+        vector<vector<int>>triangle;
+        for(int i=0;i<numRows;i++){
+            vector<int>row(i+1,1);
+            for(int j=1;j<i;j++){
+                row[j]=triangle[i-1][j-1]+triangle[i-1][j];
             }
+            triangle.push_back(row);
         }
-
-        return res;
+        return triangle;
     }
 };
 
